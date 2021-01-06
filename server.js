236f13b50cd994ced9cecd45dfcd4ac0ce73ca06
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2021-01-06 20:12:08 +1100

    original develop files + db.js + service worker but not yet functional

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://andrewmiddleton1:Gundaroo484@cluster0.ree7w.mongodb.net/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
